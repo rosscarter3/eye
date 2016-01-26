ae0faef7a8a553e62ebd9b672697175e9c75a3b3
Ross Carter <ross.carter@jic.ac.uk>
2016-01-26 18:21:06 +0000

    work on apply_correction.py

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Eye
 
 A simple command line image viewer, written in SDL.
+
+# SegCor
+`segcor.py` generates the file `merges(time).txt` which can be applied to the uncorrected image with `apply_corrections.py` to generate a corrected segmented image.
+
+# Keyboard Commands
+
+| Key  | Command  | notes |
+| ---  | -------  |
+| up   | Zoom In  |
+| down | Zoom Out |
+|left| Previous Image |
+|right| Next Image |
+|h| Move Left |
+|j| Move Up |
+|k| Move Down |
+|l| Move Right |
+|1| Select Cell 1| (segcor only) Cell under mouse pointer|
+|2| Select Cell 2| (segcor only) Cell under mouse pointer|
+|m| Merge cells 1 and 2 | (segcor only) Changes colour of cell 2 to colour of cell 1|
+|b| set cell to background | (segcor only) Change cell under mouse pointer to black|
--- lib/apply_corrections.py
+++ lib/apply_corrections.py
@@ -1,25 +1,37 @@
 """applies corrections generated from segcor.py"""
-import os
-import os.path
-import argparse
-import re
+import os, sys, argparse
 
-import ctypes
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import sys
-import ctypes
-from sdl2 import *
-from sdl2.sdlimage import IMG_LoadTexture
-import sdl2.ext
-import datetime as dt 
 
 
+def generate_cellDict(seg_im):
+    ar = mpimg.imread(seg_im)
+    
+    y,x,_ = ar.shape
+    
+    id_array = np.zeros((y, x), dtype=np.uint32)
+    
+    id_array = ar[:,:,2] + 256 * ar[:, :, 1] + 256 * 256 * ar[:, :, 0]
+    
+    cell_ids = list(np.unique(id_array))
+    
+    for ids in range(len(cell_ids)):
+	print cell_ids[ids]
+    
+    print cd
+    pass
+    
+def correct_image(seg_im,celldict):
+    pass
+    
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description=__doc__)
-	parser.add_argument("seg_im", help="Segmented Image")
-	parser.add_argument("corr", help="Corrections File")
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("seg_im", help="Segmented Image")
+    parser.add_argument("corr", help="Corrections File")
 
-	args = parser.parse_args()
-
+    args = parser.parse_args()
+    
+    celldict = generate_cellDict(args.seg_im)
+    
+    corrected_seg_im = correct_image(args.seg_im,celldict)
