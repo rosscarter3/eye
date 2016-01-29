ec27d954770709a530f5a109d92a5cd44f5be703
Ross Carter <ross.carter@jic.ac.uk>
2016-01-29 10:27:56 +0000

    apply_corrections.py now works correctly

--- lib/apply_corrections.py
+++ lib/apply_corrections.py
@@ -7,7 +7,6 @@ import matplotlib.image as mpimg
 
 class Image(object):
     """Image"""
-    
     def __init__(self,image,corr_file):
 	self.image = image
 	self.corr_file = corr_file
@@ -22,17 +21,18 @@ class Image(object):
     def apply_correction(self):
 	"""turns cell2 to colour of cell1"""
 	rgb1, rgb2 = self.rgb1, self.rgb2
-	mask = make_mask(self.im_arr, rgb2)
+	mask = make_mask(self.im_arr, rgb1)
 	self.im_arr[mask,:] = rgb2
     
     def correct_image(self):
 	"""reads merges file and corrects image"""
+	numtemp = self.im_arr
 	with open(self.corr_file, "r") as cf:
 	    for line in cf:
 		print line
 		self.rgb1, self.rgb2 = parse_line(line)
 		self.apply_correction()
-		
+	
     def save_image(self):
 	"""saves image"""
 	mpimg.imsave(self.outpath, self.im_arr)
@@ -45,19 +45,19 @@ def parse_line(string):
     
 def make_mask(array, rgb):
     """makes mask for changing cell colours"""
-	def get_mask(array,rgb,ci):
-	    y, x, _ = array.shape
-	    mask = np.zeros((y, x), dtype=bool)
-	    mask[np.where(array[:,:,ci] == rgb[ci])] = True
-	    return mask
-
-	red_mask = get_mask(array,rgb, 0)
-	green_mask = get_mask(array,rgb, 1)
-	blue_mask = get_mask(array,rgb, 2)
-	
-	mask = np.logical_and(red_mask, green_mask)
-	mask = np.logical_and(mask, blue_mask)
+    def get_mask(array,rgb,ci):
+	y, x, _ = array.shape
+	mask = np.zeros((y, x), dtype=bool)
+	mask[np.where(array[:,:,ci] == rgb[ci])] = True
 	return mask
+
+    red_mask = get_mask(array,rgb, 0)
+    green_mask = get_mask(array,rgb, 1)
+    blue_mask = get_mask(array,rgb, 2)
+	
+    mask = np.logical_and(red_mask, green_mask)
+    mask = np.logical_and(mask, blue_mask)
+    return mask
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
