egcor and apply_corrections now use PIL for correct importing/exporting (not scaling RGB values to 0->1)

lib/apply_corrections.py
@@ -3,14 +3,16 @@ import os, sys, argparse
 import re
 
 import numpy as np
-import matplotlib.image as mpimg
+from PIL import Image
 
-class Image(object):
-    """Image"""
+class Corrector(object):
+    """Image Corrector"""
     def __init__(self,image,corr_file):
 	self.image = image
 	self.corr_file = corr_file
-	self.im_arr = mpimg.imread(self.image)
+	
+	img = Image.open(self.image)
+	self.im_arr = np.array(img)
 	
 	im_name = os.path.basename(self.image)
 	im_path = os.path.dirname(self.image)
@@ -35,7 +37,7 @@ class Image(object):
 	
     def save_image(self):
 	"""saves image"""
-	mpimg.imsave(self.outpath, self.im_arr)
+	Image.fromarray(self.im_arr).save(self.outpath)
 
 def parse_line(string):
     """parses lines from read merges file"""
@@ -66,7 +68,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     
-    cor_im = Image(args.seg_im,args.corr)
+    cor_im = Corrector(args.seg_im,args.corr)
     cor_im.correct_image()
     cor_im.save_image()
     

lib/segcor.py
@@ -4,15 +4,15 @@ import os.path
 import argparse
 import re
 
-import ctypes
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
 import sys
+
 import ctypes
 from sdl2 import *
 from sdl2.sdlimage import IMG_LoadTexture
 import sdl2.ext
+
 import datetime as dt 
 
 def sorted_nicely(l):
@@ -272,7 +272,9 @@ class Viewer(object):
 	self.numpy[mask,] = cell1id
 	
 	merge_path = os.path.join(self.directory, self.im_name)
-	mpimg.imsave(merge_path, self.numpy)
+	
+	im = Image.fromarray(self.numpy)
+	im.save(merge_path)
 	
 	self._images.update_current(merge_path)
 	self.update_image()
@@ -281,7 +283,7 @@ class Viewer(object):
 
     def set_to_background(self,bcell):
 	
-	print "cell: ", self.bcid, "set to [0. 0. 0.]"
+	print "cell: ", self.bcid, "set to [0  0  0]"
 	outstring = "%s -> [ 0, 0, 0]\n"%(np.array_str(self.bcell))
 	with open(self.fp, "a") as op:
 	    op.write(outstring)
@@ -302,7 +304,8 @@ class Viewer(object):
 	self.numpy[mask] = [0,0,0]
 	    
 	merge_path = os.path.join(self.directory, self.im_name)
-	mpimg.imsave(merge_path, self.numpy)
+	im = Image.fromarray(self.numpy)
+	im.save(merge_path)
 	
 	self._images.update_current(merge_path)
 	self.update_image()
@@ -373,6 +376,7 @@ if __name__ == "__main__":
 	directory = os.path.commonprefix([args.seg_im, args.base_im])
 
 	# use PIL
-	numpy = mpimg.imread(args.seg_im)
+	img = Image.open(args.seg_im)
+	numpy = np.array(img)
 	
 	viewer = Viewer(images,numpy,directory)